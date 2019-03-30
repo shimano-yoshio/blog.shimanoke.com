0eb3c10ce88701fec56a7737ebc273b58c4b3629
Yoshio Shimano <shimano.yoshio@jp.fujitsu.com>
2019-03-30 14:31:29 +0900

    Fix a bug

--- themes/hugo-w3-simple/static/js/myscript.js
+++ themes/hugo-w3-simple/static/js/myscript.js
@@ -1,6 +1,6 @@
 function toggle_toc() {
     var x = document.getElementById("toc_contents");
-    if (x.className.indexOf("w3-show") == -1) 
+    if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
     } else {
         x.className = x.className.replace(" w3-show", "");
